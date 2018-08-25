417f68aeea6d7fef7dc669db4d806c42526034af
Felix Schlitter <felixschlitter@protonmail.com>
2018-08-25 13:12:10 +1200

    Update reference c files to compile again

--- reference/main.c
+++ reference/main.c
@@ -10,61 +10,36 @@ const Data_Show_Show * Data_Maybe_showInt () {
 		PURS_ANY_BLOCK((const purs_any_t * i) {
 			const int * value0 = purs_any_get_int(i);
 			if (value0 != NULL) {
-				return purs_any_set_string(
-					GC_NEW(purs_any_t),
-					managed_utf8str_new(afmt("%i", *value0))
-				);
+				return PURS_ANY_STRING(afmt("%i", *value0));
 			}
 			assert (0); // XXX what now?
 		});
 	return value0;
 }
 
-typedef struct purs_record {
-    purs_any_t * key;
-    purs_any_t * value;
-    UT_hash_handle hh;
-} purs_record_t;
-
-
-#define PURS_RECORD_ADD_MUT($record, $key, $value) \
-	do { \
-		purs_record_t * entry = GC_NEW(purs_record_t); \
-		entry->key = PURS_ANY_STRING($key); \
-		entry->value = $value; \
-		HASH_ADD_PTR($record, key, entry); \
-	} while (0)
-
-const purs_record_t ** purs_record_copy(const purs_record_t **) {
-
-}
-
-const purs_record_t ** make_record_foo () {
-	purs_record_t ** record = GC_NEW(purs_record_t *);
-	PURS_RECORD_ADD_MUT(*record, "foobar", PURS_ANY_INT(100));
-	return (const purs_record_t **) record;
-}
-
-// XXX: do this properly in amortized O(1)
-const purs_any_t * purs_record_find_by_key(const purs_record_t ** record, void * key) {
-	const purs_record_t * current_entry, * tmp;
-	HASH_ITER(hh, *record, current_entry, tmp) {
-		if (purs_any_eq_string(current_entry->key, key)) {
-			return current_entry->value;
-		}
-	}
-	return NULL;
+void foobar () {
+	do {
+		/* purs_vec_t * xs = purs_vec_new(); */
+		/* vec_pusharr(xs, (const purs_any_t * [1]) { a }, 1); */
+	} while (0);
 }
 
 int main () {
 	const purs_any_t * a = Data_Maybe_Just(foo);
 	const Data_Show_Show * x = Data_Maybe_showMaybe(Data_Maybe_showInt());
 	const purs_any_t * y = purs_any_app(x->show, a);
-	printf("%s\n", (char *) y->value.string->data);
 
-	const purs_record_t ** r = make_record_foo();
-	const purs_any_t * o = purs_record_find_by_key(r, (void *) "foobar");
-	printf("%d\n", *purs_any_get_int(o));
+	printf("%s\n", (char *) purs_any_get_string(y)->data);
+
+	const purs_vec_t * v = purs_vec_new((const purs_any_t*[1]) { y }, 1);
+	const purs_any_t * i = v->data[0];
+
+	printf("%s\n", (char *) purs_any_get_string(i)->data);
+
+	const purs_vec_t * v2 = purs_vec_copy(v);
+	const purs_any_t * i2 = v2->data[0];
+
+	printf("%s\n", (char *) purs_any_get_string(i2)->data);
 
 	return 0;
 }
--- reference/output/Example.c
+++ reference/output/Example.c
@@ -1,10 +1,3 @@
 #include "Example.h"
 
 PURS_ANY_THUNK_DEF(foo, PURS_ANY_INT(100))
-
-PURS_ANY_THUNK_DE(bar,
-	PURS_ANY_BLOCK(() {
-
-		return NULL;
-	})
-)
