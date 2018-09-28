bd945e3be1ccb2146a5e114da211a2ea146e56ab
Lupino <lmjubuntu@gmail.com>
2018-09-29 07:55:22 +0800

    add more array test

--- examples/foldable-traversable/Main.purs
+++ examples/foldable-traversable/Main.purs
@@ -8,14 +8,27 @@ import Data.Foldable (foldr, foldl)
 import Data.FunctorWithIndex (mapDefault)
 import Data.Traversable (traverse)
 
+run :: forall a. Show a => String -> a -> Effect Unit
+run label result = Console.log $ "[" <> label <> "]: " <> show result
+
+oneTest :: Array Int -> Effect Unit
+oneTest arr = do
+  Console.log $ "test with input: " <> show arr
+  run "foldr" $ foldr (+) 0 arr
+  run "foldl" $ foldl (+) 0 arr
+  run "mapDefault" $ mapDefault (\i -> i + 1) arr
+  run "traverse" =<< traverse pure arr
+
+
 main :: Effect Unit
 main = do
-  run "foldr" $ foldr (+) 0 [1,2,3,4,5,6,7,8,9]
-  run "foldl" $ foldl (+) 0 [1,2,3,4,5,6,7,8,9]
-  run "mapDefault" $ mapDefault (\i -> i + 1) [1,2,3,4,5,6,7,8,9]
-  run "traverse" =<< traverse pure [1,2,3,4,5,6,7,8,9]
-  where
-  run :: forall a. Show a => String -> a -> Effect Unit
-  run label result =
-    Console.log $
-      "[" <> label <> "]: " <> show result
+  oneTest []
+  oneTest [1]
+  oneTest [1, 2]
+  oneTest [1, 2, 3]
+  oneTest [1, 2, 3, 4]
+  oneTest [1, 2, 3, 4, 5]
+  oneTest [1, 2, 3, 4, 5, 6]
+  oneTest [1, 2, 3, 4, 5, 6, 7]
+  oneTest [1, 2, 3, 4, 5, 6, 7, 8]
+  oneTest [1, 2, 3, 4, 5, 6, 7, 8, 9]
