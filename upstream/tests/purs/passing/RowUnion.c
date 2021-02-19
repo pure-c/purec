c173549f49950b9e40ada65bbbcebbd2e1097ed2
Felix Schlitter <felixschlitter@gmail.com>
2021-02-20 08:56:13 +1300

    Update more upstream tests

--- upstream/tests/purs/passing/RowUnion.c
+++ upstream/tests/purs/passing/RowUnion.c
@@ -1,7 +1,10 @@
 #include <purescript.h>
 
 PURS_FFI_FUNC_3(Main_merge, _, _l, _r) {
-	const purs_record_t * l = purs_any_get_record(_l);
-	const purs_record_t * r = purs_any_get_record(_r);
-	return purs_any_record_new(purs_record_merge(l, r));
+	const purs_record_t *l = purs_any_force_record(_l);
+	const purs_record_t *r = purs_any_force_record(_r);
+	purs_any_t ret = purs_any_record(purs_record_merge(l, r));
+	PURS_RC_RELEASE(l);
+	PURS_RC_RELEASE(r);
+	return ret;
 }
